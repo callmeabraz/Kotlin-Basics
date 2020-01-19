eea2cceecfa5155e5cb4659a5e1d0e0b6349bc30
Mohammed Abraz <callmeabraz@gmail.com>
2020-01-19 21:04:52 +0400

    Data Type Fundamentals of Kotlin

---
+++ src/DataTypes.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    //Declaring a variable - 1
+    val str = "Welcome to Kotlin, Abraz"
+    println(str)
+
+    //Declaring a variable - 2
+    val str2:String
+    str2 = "Welcome Abraz"
+    println(str2)
+
+    //Printing with templates
+    val name = "Abraz"
+    println("Name: $name")
+    // use val for constant variables
+    // use var for other variables
+    
+
+}
